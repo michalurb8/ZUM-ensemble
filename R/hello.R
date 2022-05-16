af599b7e2ab93c83265426bf7d4d7b12701635c2
Michal Urbanski <michalurb8@gmail.com>
2022-05-16 22:39:46 +0200

    First commit

---
+++ R/hello.R
@@ -0,0 +1,10 @@
+require(naivebayes)
+ens <- function(model, x, y, n) {
+  results = c()
+  for (i in 1:n) {
+
+  }
+  naiveBayes(x, y)
+}
+
+model <- ens(naiveBayes, c(1,2,3,4), c("dupa", "cupa"), 3)
